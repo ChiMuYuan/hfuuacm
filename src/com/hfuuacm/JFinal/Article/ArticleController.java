a47d5fce1d0edb06a86309c10712ca190592be2a
Shilin Yuan <1486960657@qq.com>
2018-09-12 15:38:29 +0800

    bug_fix

--- src/com/hfuuacm/JFinal/Article/ArticleController.java
+++ src/com/hfuuacm/JFinal/Article/ArticleController.java
@@ -3,10 +3,15 @@ package com.hfuuacm.JFinal.Article;
 import com.hfuuacm.JFinal.Mysql.Article;
 import com.hfuuacm.JFinal.Mysql.Subject;
 import com.hfuuacm.JFinal.Mysql.User;
+import com.jfinal.aop.Before;
 import com.jfinal.core.Controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Before(ArticleInterceptors.class)
 public class ArticleController extends Controller {
     public void index() { redirect("/");}
 
@@ -16,23 +21,24 @@ public class ArticleController extends Controller {
         String number = getPara("lists");
         int stid = 0, all_page = 0;
 
-        List<Article> all_articleList = null;
+        List<Article> all_articleList;
         if (column != null) {
             if (page != null)
                 stid += 10 * Integer.parseInt(page);
 
-            Subject subject = Subject.dao.findFirst("SELECT * FROM article WHERE topic=?", column);
+            Subject subject = Subject.dao.findFirst("SELECT * FROM subject WHERE topic=?", column);
             all_page = subject.getInt("number");
             all_articleList = Article.dao.find("SELECT * FROM article WHERE subject=? ORDER BY id LIMIT ?,?",
-                    subject.getStr("id"), stid, number);
+                    subject.getStr("id"), stid, Integer.parseInt(number));
         }
         else
-            all_articleList = Article.dao.find("SELECT * FROM article WHERE subject=? ORDER BY id LIMIT ?", number);
+            all_articleList = Article.dao.find("SELECT * FROM article WHERE subject=? ORDER BY id LIMIT ?",
+                    Integer.parseInt(number));
 
-        List<Object> articleList = null;
+        List<Object> articleList = new ArrayList<>();
         for (int i = 0; i < all_articleList.size(); i ++) {
             Article article = all_articleList.get(i);
-            Map<Object, Object> articleMap = null;
+            Map<Object, Object> articleMap = new HashMap<>();
             articleMap.put("id", article.getStr("id"));
             articleMap.put("title", article.getStr("title"));
             articleMap.put("time", article.getStr("Lasttime"));
@@ -40,7 +46,7 @@ public class ArticleController extends Controller {
             articleList.add(articleMap);
         }
 
-        Map<Object, Object> Json = null;
+        Map<Object, Object> Json = new HashMap<>();
         Json.put("status", "success");
         Json.put("article", articleList);
         Json.put("all_page", Math.ceil(all_page / 10.0));
