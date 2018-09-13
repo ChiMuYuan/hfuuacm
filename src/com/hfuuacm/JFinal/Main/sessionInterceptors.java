9c7aeba444ce88ac46ff0a1821b9b93e62072095
Shilin Yuan <1486960657@qq.com>
2018-09-13 16:56:31 +0800

    bug fix

--- src/com/hfuuacm/JFinal/Main/sessionInterceptors.java
+++ src/com/hfuuacm/JFinal/Main/sessionInterceptors.java
@@ -17,7 +17,8 @@ public class sessionInterceptors implements Interceptor {
             if (uid != null && auth_token != null) {
                 User user = User.dao.findById(uid);
 
-                if (user != null && user.getStr("auth_token").equals(auth_token)) {
+                if (user != null && user.getStr("auth_token") != null &&
+                        user.getStr("auth_token").equals(auth_token)) {
                     controller.setSessionAttr("uid", user.getStr("id"));
                     controller.setSessionAttr("Username", user.getStr("Username"));
                     controller.setSessionAttr("permission", user.getStr("permission"));
